8b6fc3e91e092ecc56bc29aee43fdfefc92d7f2e
rodrigo <rodrigo@192.168.0.117>
2017-04-10 15:10:10 -0300

    te odio revert commit.

--- SistemaSushi-ejb/build/generated-sources/ap-source-output/pojos/Nivelusuario_.java
+++ SistemaSushi-ejb/build/generated-sources/ap-source-output/pojos/Nivelusuario_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import pojos.Usuario;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-09T21:31:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-10T15:07:03")
 @StaticMetamodel(Nivelusuario.class)
 public class Nivelusuario_ { 
 
--- SistemaSushi-ejb/build/generated-sources/ap-source-output/pojos/Pedido_.java
+++ SistemaSushi-ejb/build/generated-sources/ap-source-output/pojos/Pedido_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import pojos.Producto;
 import pojos.Usuario;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-09T21:31:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-10T15:07:03")
 @StaticMetamodel(Pedido.class)
 public class Pedido_ { 
 
